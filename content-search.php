9aa52b90c5acd1dfa0f83b86dc8c4bb516afe93c
James Apoefis <james.apoefis@yahoo.com.au>
2017-07-16 16:45:07 +0930

    customised index.php for blog post summary

--- content-search.php
+++ content-search.php
@@ -1,9 +1,16 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-	<?php the_title("<h1 class='entry-title'>", "</h1>"); ?>
 
-	<small><?php the_category(''); ?> || <?php the_tags(); ?> || <?php edit_post_link(); ?></small>
+	<?php
+		global $wp_query;
+		$total_results = $wp_query->found_posts;
 
-	<?php the_excerpt(); ?>
+		if ($total_results > 0) {
+			echo the_title("<h1 class='entry-title'>", "</h1>");
+			the_excerpt();	
+		} else {
+			echo "This is here";
+		} 
+
+	?>
 
 </div>
