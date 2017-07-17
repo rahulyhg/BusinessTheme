51b863b19ab3268b881ee0f52f7648ba0de5751f
James Apoefis <james.apoefis@yahoo.com.au>
2017-07-17 19:08:19 +0930

    added marjup for font-page

--- front-page.php
+++ front-page.php
@@ -1,6 +1,116 @@
-<?php get_header(); ?>
+		<?php get_header(); ?>
 
 
-		
+		<!--******************************************************* Start Content Section *******************************************************-->
+				
+		<div class="container">
+			
+			<div class="row">
+				
+				<div class="col-md-5">
+						
+					<h1>Company Name</h1>
+					<h2>Welcome</h2>
 
-<?php get_footer(); ?>
+				</div>
+
+				<div class="col-md-7">
+					
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt maxime aspernatur delectus adipisci dicta ipsam quo, odit, fugit similique porro maiores, cum praesentium aliquid sequi. Doloribus animi, architecto laborum!. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis ratione nisi facere eaque aperiam labore, suscipit pariatur reiciendis corrupti voluptatibus? Pariatur voluptates voluptatibus nam adipisci dignissimos! Error, iusto assumenda aperiam?. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem molestias, cum, sit velit ducimus fuga at tenetur eos officiis architecto explicabo, voluptates repellat veniam aperiam ipsa expedita itaque animi veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia aspernatur esse cumque in delectus eum, maiores eveniet deleniti recusandae velit quam aut et, porro iste! Quibusdam quos beatae et, illo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis optio, sunt cupiditate aperiam nemo eius, saepe amet. Natus mollitia, quasi ipsam libero eligendi in, fuga quibusdam numquam asperiores, repudiandae commodi.</p>
+
+				</div>
+
+			</div>
+
+		</div>
+
+
+		<!--******************************************************* Start Tiles Section *******************************************************-->
+
+		<div class="tileWrapper">
+
+			<div class="container">
+				
+				<div class="row">
+					
+					<div class="col-md-4">
+						
+						<h2>Product One</h2>
+						<img class="fluid" src="<?php echo get_template_directory_uri();?>/assets/images/tile_1.jpg" alt="Product One">
+						<a href="#"><p>Learn More</p></a>
+
+					</div>
+
+					<div class="col-md-4">
+						
+						<h2>Product One</h2>
+						<img class="fluid" src="<?php echo get_template_directory_uri();?>/assets/images/tile_2.jpg" alt="Product One">
+						<a href="#"><p>Learn More</p></a>	
+					
+					</div>
+
+					<div class="col-md-4">
+						
+						<h2>Product One</h2>
+						<img class="fluid" src="<?php echo get_template_directory_uri();?>/assets/images/tile_3.jpg" alt="Product One">
+						<a href="#"><p>Learn More</p></a>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+
+		<!--******************************************************* Start Services Section *******************************************************-->
+
+		<div class="container">
+				
+			<div class="row">
+				
+				<div class="col-md-12">
+					
+					<h1>Our Services</h1>
+
+				</div>
+
+			</div>
+
+			<div class="row">
+				
+				<div class="col-md-4">
+					
+					<img src="<?php echo get_template_directory_uri();?>/assets/images/service_1.jpg" alt="Service One">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid saepe nesciunt est nemo, provident dolorem aperiam a molestiae magni nulla maxime porro, esse, ducimus, ipsum dolores repellat itaque iusto vitae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam velit iusto explicabo facilis, ea, itaque autem rem cum quam aliquid voluptate alias quibusdam doloribus vitae amet, eaque aliquam consequatur eius.</p>
+
+				</div>
+
+				<div class="col-md-4">
+					
+					<img src="<?php echo get_template_directory_uri();?>/assets/images/service_2.jpg" alt="Service Two">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid saepe nesciunt est nemo, provident dolorem aperiam a molestiae magni nulla maxime porro, esse, ducimus, ipsum dolores repellat itaque iusto vitae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam velit iusto explicabo facilis, ea, itaque autem rem cum quam aliquid voluptate alias quibusdam doloribus vitae amet, eaque aliquam consequatur eius.</p>
+
+				</div>
+
+				<div class="col-md-4">
+					
+					<img src="<?php echo get_template_directory_uri();?>/assets/images/service_3.jpg" alt="Service Three">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid saepe nesciunt est nemo, provident dolorem aperiam a molestiae magni nulla maxime porro, esse, ducimus, ipsum dolores repellat itaque iusto vitae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam velit iusto explicabo facilis, ea, itaque autem rem cum quam aliquid voluptate alias quibusdam doloribus vitae amet, eaque aliquam consequatur eius.</p>
+
+				</div>
+
+			</div>
+
+		</div>
+
+
+
+		<!--******************************************************* Start Services Section *******************************************************-->
+
+		<iframe width="100%" height="450px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJcdtXHpqvD4gR4pTkJQDGZHU&key=AIzaSyB0MY76SOV79MCuyV1SGLLsHghkvRvJPIQ " allowfullscreen></iframe> 
+
+
+
+		<?php get_footer(); ?>
