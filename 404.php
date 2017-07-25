e1fad35d249dc8b1563892bba0831a2d40112142
James Apoefis <james.apoefis@yahoo.com.au>
2017-07-25 21:08:41 +0930

    added 404.php page

---
+++ 404.php
@@ -0,0 +1,23 @@
+		<?php get_header(); ?>
+
+
+		<!--******************************************************* Start Content Section *******************************************************-->
+		
+		<div class="container">
+			
+			<div class="row">
+				
+				<div class="col-md-12">
+					
+					<p>Sorry this page could not be found.</p>
+
+					<p>Try heading to the <a href="<?php echo get_home_url(); ?>">home page</a> and starting again</p>
+
+				</div>
+
+			</div>
+
+		</div>
+
+
+		<?php get_footer(); ?>
